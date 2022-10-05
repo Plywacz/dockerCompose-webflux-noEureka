58ad823facf0b49f654fee04d78b24c63066c9cb
Plywacz <m.plywacz@interia.pl>
2022-10-05 12:15:50 +0200

    BookService.java clean import + format code

--- src/main/java/org/plywacz/client/BookService.java
+++ src/main/java/org/plywacz/client/BookService.java
@@ -3,14 +3,11 @@ package org.plywacz.client;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.Collection;
 import java.util.Random;
 import java.util.stream.Collectors;
-
-import static java.util.Objects.requireNonNull;
 
 @Service
 @AllArgsConstructor

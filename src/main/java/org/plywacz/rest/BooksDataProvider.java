cd48cd4a64e4ded29b97c3c2f9b5dde65289a183
Plywacz <m.plywacz@interia.pl>
2022-10-05 12:44:36 +0200

    simulating fetching data from db -> thread sleep 1 sec on getting books data

--- src/main/java/org/plywacz/rest/BooksDataProvider.java
+++ src/main/java/org/plywacz/rest/BooksDataProvider.java
@@ -1,5 +1,6 @@
 package org.plywacz.rest;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Component
+@Log4j2
 class BooksDataProvider {
     private final List<Book> books;
 
@@ -19,6 +21,16 @@ class BooksDataProvider {
     }
 
     public Collection<Book> all() {
+        simulateHeavyDataSelect();
         return Collections.unmodifiableList(books);
+    }
+
+    private void simulateHeavyDataSelect() {
+        log.always().log("simulating fetching BOOK data from DB: takes 1 sec");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            log.always().log("Thread.sleep(1000) error", e);
+        }
     }
 }

2ceb2bada417d67a445c78349fd42181932e46a8
Plywacz <m.plywacz@interia.pl>
2022-10-12 15:30:48 +0200

    reimpl data model to be consistent with authors rest microservice

--- src/main/java/org/plywacz/client/ExtendedBook.java
+++ src/main/java/org/plywacz/client/ExtendedBook.java
@@ -1,7 +1,12 @@
 package org.plywacz.client;
 
-record ExtendedBook(Book coreBook, String someAdditionalData) {
+record ExtendedBook(Book coreBook, Author author, String someAdditionalData) {
 }
-record Book(String title, String author, int year) {
+
+record Book(String title, long authorId) {
 }
+
+record Author(String name, long id) {
+}
+
 

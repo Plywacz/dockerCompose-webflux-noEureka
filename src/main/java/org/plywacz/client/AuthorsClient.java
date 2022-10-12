ec51300bc9a14cf7744620f08ca499ef40c9f217
Plywacz <m.plywacz@interia.pl>
2022-10-12 16:02:20 +0200

    fetching method is blocking since rest doesn't support web-flux streaming ,renamed method

--- src/main/java/org/plywacz/client/AuthorsClient.java
+++ src/main/java/org/plywacz/client/AuthorsClient.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -15,23 +13,23 @@ import java.util.List;
 class AuthorsClient {
     private final WebClient webClient;
 
-    AuthorsClient(@Value("${booksProviderUrl}") String authorsProviderUrl) {
+    AuthorsClient(@Value("${authorsProviderUrl}") String authorsProviderUrl) {
         this.webClient = WebClient.create(authorsProviderUrl);
     }
 
 
-    Mono<Collection<Author>> fetchAllBooks() {
+    List<Author> fetchAllAuthors() {
         return webClient
                 .get()
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Collection<Author>>() {
+                .bodyToMono(new ParameterizedTypeReference<List<Author>>() {
                 })
                 .onErrorReturn(error -> {
                             log.error("Error during fetching data from Authors rest api", error);
                             return true;
                         },
                         List.of()
-                );
+                ).block();
 
     }
 }
